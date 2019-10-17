a2840740571ecda753eea6c3a9b786cd9f8d7983
Krister <37347340+SeventhDisaster@users.noreply.github.com>
2019-10-17 12:25:12 +0200

    Adjusted test and TaskDao methods for actual database queries

--- src/main/java/no/kristiania/dao/TaskDao.java
+++ src/main/java/no/kristiania/dao/TaskDao.java
@@ -1,8 +1,10 @@
 package no.kristiania.dao;
 
+import javax.sql.ConnectionEvent;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -19,9 +21,8 @@ public class TaskDao {
   public void insert(String taskName) {
     taskList.add(taskName);
 
-
     try (Connection conn = datasource.getConnection()) {
-      PreparedStatement statement = conn.prepareStatement("INSERT INTO tablename (colonName) VALUES (?)");
+      PreparedStatement statement = conn.prepareStatement("INSERT INTO tasks (name) VALUES (?)");
       statement.setString(1, taskName);
       statement.executeUpdate();
     } catch (SQLException e) {
@@ -30,7 +31,20 @@ public class TaskDao {
 
   }
 
-  public List<String> listAll() {
-    return taskList;
+  //This method returns all values in a certain column
+  public List<String> listAll(String columnName) throws SQLException {
+    List<String> result = new ArrayList<>();
+    try(Connection connection = datasource.getConnection()){
+      try(PreparedStatement statement = connection.prepareStatement(
+              "SELECT * FROM tasks"
+      )) {
+        try(ResultSet rs = statement.executeQuery()){
+          while(rs.next()){
+            result.add(rs.getString(columnName));
+          }
+          return result;
+        }
+      }
+    }
   }
 }
--- src/test/java/no/kristiania/dao/TaskDaoTest.java
+++ src/test/java/no/kristiania/dao/TaskDaoTest.java
@@ -3,12 +3,18 @@ package no.kristiania.dao;
 import org.h2.jdbcx.JdbcDataSource;
 import org.junit.jupiter.api.Test;
 
+import java.sql.SQLException;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class TaskDaoTest {
   @Test
-  void shouldReturnTaskList(){
+  void shouldReturnTaskList() throws SQLException {
     JdbcDataSource datasource = new JdbcDataSource();
+    datasource.setUrl("jdbc:h2:mem:testDataBase");
+    datasource.getConnection().createStatement().executeUpdate(
+            "CREATE TABLE tasks (name varchar(100))"
+    );
     //TODO: set up h2 database for test
     TaskDao dao = new TaskDao(datasource);
     dao.insert("Make a Database");
