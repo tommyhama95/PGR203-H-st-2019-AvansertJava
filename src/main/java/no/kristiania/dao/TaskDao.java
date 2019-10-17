96254e8227d46a55040ddb2f6e13a133bd1feedf
Tommyhama95 <tommyhamarsnes@gmail.com>
2019-10-17 11:57:40 +0200

    shouldreturnTaskList passed

--- src/main/java/no/kristiania/dao/TaskDao.java
+++ src/main/java/no/kristiania/dao/TaskDao.java
@@ -1,12 +1,17 @@
 package no.kristiania.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TaskDao {
+
+  private List<String> taskList = new ArrayList<>();
+
   public void insert(String make_a_database) {
+    taskList.add(make_a_database);
   }
 
   public List<String> listAll() {
-    return null;
+    return taskList;
   }
 }
