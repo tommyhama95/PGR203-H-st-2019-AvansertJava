76900dcab5536dba6eaccb6ff18ad764dc848bcf
Tommyhama95 <tommyhamarsnes@gmail.com>
2019-10-30 16:48:42 +0100

    Created client and clientResponse
    * getting status codes

--- src/main/java/no/kristiania/http/HttpClient.java
+++ src/main/java/no/kristiania/http/HttpClient.java
@@ -1,15 +1,42 @@
 package no.kristiania.http;
 
-public class HttpClient {
-  public HttpClient(String host, int port, String requestTarget) {
+import java.io.IOException;
+import java.net.Socket;
 
+public class HttpClient {
+  private String host;
+  private int port;
+  private String requestTarget;
+  private HttpClientRespone clientRespone;
+
+
+  public HttpClient(String host, int port, String requestTarget) {
+    this.host = host;
+    this.port = port;
+    this.requestTarget = requestTarget;
   }
 
-  public void executeRequest() {
+  public void executeRequest() throws IOException {
+    Socket socket = new Socket(host, port);
+    socket.getOutputStream().write(("GET " + requestTarget +" HTTP/1.1\r\n").getBytes());
+    socket.getOutputStream().write(("Host: " + host +"\r\n").getBytes());
+    socket.getOutputStream().write(("Connection: close\r\n").getBytes());
+    socket.getOutputStream().write(("\r\n").getBytes());
+    socket.getOutputStream().flush();
+
+    StringBuilder response = new StringBuilder();
+    int c;
+    while((c = socket.getInputStream().read()) != -1){
+      response.append((char)c);
+    }
+    System.out.println(response.toString());
+
+    clientRespone = new HttpClientRespone(response.toString());
+
+
   }
 
   public int getStatusCode() {
-
-    return 200;
+    return clientRespone.getStatusCode();
   }
 }
--- src/test/java/no/kristiania/http/HttpClientTest.java
+++ src/test/java/no/kristiania/http/HttpClientTest.java
@@ -2,19 +2,21 @@ package no.kristiania.http;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HttpClientTest {
 
   @Test
-  void shouldReturnStatusCode200(){
+  void shouldReturnStatusCode200() throws IOException {
     HttpClient client = new HttpClient("urlecho.appspot.com",80,"/echo?status=200");
     client.executeRequest();
     assertEquals(200,client.getStatusCode());
   }
 
   @Test
-  void shouldReturnErrorCode404(){
+  void shouldReturnErrorCode404() throws IOException {
     HttpClient client = new HttpClient("urlecho.appspot.com",80,"/echo?status=404");
     client.executeRequest();
     assertEquals(404, client.getStatusCode());
