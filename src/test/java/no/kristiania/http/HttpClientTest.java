766dafbac210925dbe6543243229fdfa816ee2ad
Krister <37347340+SeventhDisaster@users.noreply.github.com>
2019-10-30 16:21:09 +0100

    Added failing test to HTTPClient

--- src/test/java/no/kristiania/http/HttpClientTest.java
+++ src/test/java/no/kristiania/http/HttpClientTest.java
@@ -5,10 +5,19 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HttpClientTest {
+
   @Test
   void shouldReturnStatusCode200(){
-    HttpClient client = new HttpClient();
+    HttpClient client = new HttpClient("urlecho.appspot.com",80,"/echo?status=200");
     client.executeRequest();
     assertEquals(200,client.getStatusCode());
+  }
+
+  @Test
+  void shouldReturnErrorCode404(){
+    HttpClient client = new HttpClient("urlecho.appspot.com",80,"/echo?status=404");
+    client.executeRequest();
+    assertEquals(404, client.getStatusCode());
+
   }
 }
